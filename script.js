c6974a5b3ac14ed632bb093c9f32b57cd3616052
MoraisGustavo1 <gmoraisildefonso@gmail.com>
2024-04-30 16:17:43 -0300

    Criada função começarQuiz - Miguel Carmo

--- script.js
+++ script.js
@@ -35,6 +35,7 @@ function Login(){
         alert("Senha ou Usuário incorretos")
     }
 }
+
 function Alerta(){
     let nome = document.getElementById("idNome").value;
     let email = document.getElementById("idEmail").value;
@@ -46,4 +47,42 @@ function Alerta(){
     else{
         alert("Dados Enviados!")
     }
+}
+
+function comecarQuiz(){
+    let perguntas = [
+        "1. Qual é a invenção que deu origem à bicicleta moderna?",
+        "2. Em que ano a primeira bicicleta foi inventada?",
+        "3. Quem é considerado o pai da bicicleta?",
+        "4. Qual é o nome da parte da bicicleta onde você senta?",
+        "5. Como são chamados os pneus especiais usados em bicicletas de montanha?",
+        "6. Qual é o nome do sistema de marchas da bicicleta?",
+        "7. Quantas marchas tem uma bicicleta?",
+        "8. Que parte da bicicleta é usada para parar?",
+        "9. Qual é o nome do suporte onde você coloca o pé para descansar enquanto pedala?",
+        "10. Como é chamada a estrutura que conecta o guidão à bicicleta?"
+    ];
+    let respostas = [
+        "Draisiana",
+        "1817",
+        "Karl Dias",
+        "Banco",
+        "Off-road",
+        "Cambio",
+        "21",
+        "Freios",
+        "Pedal",
+        "Mesa"
+    ];
+    let pontos = 0;
+
+    for(let i = 0; i < perguntas.length; i++){
+        let usuarioResposta = prompt(perguntas[i]);
+        if (usuarioResposta.toLocaleLowerCase() === respostas[i].toLocaleLowerCase()){
+            pontos++;
+        }
+
+    }
+    let resultado = "Você acertou " + pontos + " de " + perguntas.length + " perguntas.";
+    alert(resultado)
 }
